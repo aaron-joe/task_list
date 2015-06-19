a2aa5e05e7e838ed8660a297e2cc14a312ccebf2
Joe Reiff <jcreiff@gmail.com>
2015-06-19 16:13:49 -0400

    checkbox successfully updating completed field

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,14 +37,10 @@ class TasksController < ApplicationController
   # PATCH/PUT /tasks/1
   # PATCH/PUT /tasks/1.json
   def update
-    respond_to do |format|
-      if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(task_params)
+      render partial: 'task', locals: {task: @task}
+    else
+      render :edit
     end
   end
 
