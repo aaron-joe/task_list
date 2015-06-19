f89dbb320ecd2b8eb16d90fdfee7e2d624a4a07b
Aaron Wiggins <apw7177@gmail.com>
2015-06-19 15:33:05 -0400

    work toward checkbox changing completed field

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -66,6 +66,6 @@ class TasksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
-      params.require(:task).permit(:name, :start_on, :due_on, :completed, :order_number)
+      params.require(:task).permit(:id, :name, :start_on, :due_on, :completed, :order_number)
     end
 end
